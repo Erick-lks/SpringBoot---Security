7f6c02dcb7871265bee8dadf9b665cf8e36d4bb6
Erick Caetano <erick.caetano1200@gmail.com>
2026-04-12 21:09:18 -0300

    Atualização da Camada de Security

---
+++ src/main/java/com/Seguranca/SpringBoot/Config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.Seguranca.SpringBoot.Config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configurable
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+   @Bean
+public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+       return http
+        .csrf(csrf -> csrf.disable())
+        .cors(cors -> {})
+        .sessionManagement(session ->
+            session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        )
+        .authorizeHttpRequests(authorize -> authorize
+            .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+            .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+            .anyRequest().authenticated()
+        )
+        .build();
+
+
+    }
+
+     @Bean  
+       public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+       }
+}
--- src/main/java/com/Seguranca/SpringBoot/Controller/AuthController.java
+++ src/main/java/com/Seguranca/SpringBoot/Controller/AuthController.java
@@ -2,6 +2,10 @@ package com.Seguranca.SpringBoot.Controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,29 +26,37 @@ public class AuthController {
   
     
     private final UserRepository userRepository;
+   private final PasswordEncoder passwordEncoder;
+   private final AuthenticationManager authenticationManager;
 
-      public AuthController (UserRepository userRepository){
+      public AuthController (UserRepository userRepository, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager){
         this.userRepository = userRepository;
+        this.passwordEncoder= passwordEncoder;
+        this.authenticationManager=authenticationManager;
       }
     
   
     @PostMapping("/login")
     public ResponseEntity<LoginResponseUser> login(@Valid @RequestBody LoginRequestUser request){
+
+      UsernamePasswordAuthenticationToken userandPass = new UsernamePasswordAuthenticationToken(request.email(),request.password());
+      Authentication authentication = authenticationManager.authenticate(userandPass);
+      
         return null;
     }
     
 
-    
+    @PostMapping("/register")
     public ResponseEntity<RegisterUserResponse> register (@Valid @RequestBody RegisterRequestUser request){
       User newUser = new User();
-      newUser.setPassword(request.password());
+      newUser.setPassword(passwordEncoder.encode(request.password()));
       newUser.setEmail(request.email());
       newUser.setName(request.name());
       
       userRepository.save(newUser);
 
 
-      
+  
         return ResponseEntity.status(HttpStatus.CREATED)
         .body(new RegisterUserResponse(newUser.getName() ,newUser.getEmail()));
     }
